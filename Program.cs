1aa7943b8fd0eb0e7fe1d61daed1c4476f3186a5
Fatih Guler <fatih.guler.1995@gmail.com>
2019-06-09 19:46:27 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NETCore_SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var _context = new BloggingContext())
+            {
+                _context.Blogs.Add(new Blog{ Url = "www.github.com"});
+                var count = _context.SaveChanges();
+                Console.WriteLine("{0} kayıt veritabanına eklendi", count);
+
+                Console.WriteLine();
+                foreach (var blog in _context.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
